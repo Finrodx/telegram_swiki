77d042737c5de9c04666ea59e415359851bd39d5
Salih Atacan Karagöz <salihkaragoz@std.iyte.edu.tr>
2017-07-27 02:24:41 +0300

    Update tlgrm.py

--- tlgrm.py
+++ tlgrm.py
@@ -1,17 +1,49 @@
-from telegram.ext import Updater, CommandHandler
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from wiki_search import wiki_search
+import json
+import logging
+
+updater = Updater('TOKEN')
+
+dispatcher = updater.dispatcher
 
 def start(bot, update):
-    update.message.reply_text('Hello World!')
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('231748188:AAFimbB_JwMpfGqckNM_8lYFhLtHkn8zlZo')
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+def echo(bot, update):
+
+
+
+
+    user_input = update.message.text
+
+
+
+    if user_input.split(" ", 1)[0].lower() == "wiki":
+
+        bot.send_message(chat_id=update.message.chat_id, text=wiki_search(user_input.split(" ", 1)[1]))
+
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="error ")
+
+
+
+
+
+
+
+
+start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text, echo)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+
 
 
 updater.start_polling()
-updater.idle()T
+updater.idle()
